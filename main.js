cb939973b05ab65b2d423722f6a42ce756f39def
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-25 21:59:13 +0530

    Set width and height for grid container

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ for (let i = 0; i < GRID_SIZE; i++) {
   for (let j = 0; j < GRID_SIZE; j++) {
     const innerContainer = document.createElement("div");
     innerContainer.className = "inner-container";
+    innerContainer.style.width = `${560 / GRID_SIZE}px`;
+    innerContainer.style.height = `${560 / GRID_SIZE}px`;
     gridContainer.appendChild(innerContainer);
   }
 }
