9dbc74c747c4cc70e85b4cb6dbbf967ddee79953
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-25 22:18:16 +0530

    Updated grid height and grid width values in js

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const gridContainer = document.querySelector(".grid-container");
 
 const GRID_SIZE = 16;
-const GRID_WIDTH = getComputedStyle(document.body).getPropertyValue(
-  "--grid-width"
+const GRID_WIDTH = Number(
+  getComputedStyle(document.body)
+    .getPropertyValue("--grid-width")
+    .replace("px", "")
+);
+const GRID_HEIGHT = Number(
+  getComputedStyle(document.body)
+    .getPropertyValue("--grid-height")
+    .replace("px", "")
 );
 
-const GRID_HEIGHT = getComputedStyle(document.body).getPropertyValue(
-  "--grid-height"
-);
+console.log(GRID_HEIGHT);
 
 for (let i = 0; i < GRID_SIZE; i++) {
   for (let j = 0; j < GRID_SIZE; j++) {
     const innerContainer = document.createElement("div");
     innerContainer.className = "inner-container";
-    innerContainer.style.width = `${GRID_WIDTH / GRID_SIZE}px`;
     innerContainer.style.height = `${GRID_HEIGHT / GRID_SIZE}px`;
+    innerContainer.style.width = `${GRID_WIDTH / GRID_SIZE}px`;
     gridContainer.appendChild(innerContainer);
   }
 }
