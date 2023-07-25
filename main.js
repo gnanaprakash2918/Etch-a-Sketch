cfd4e46f935f0f5361acd9984aa086d65152b2b4
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-25 20:16:50 +0530

    Replace class selector with query selector

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const gridContainer = document.getElementsByClassName("grid-container");
+const gridContainer = document.querySelector(".grid-container");
 const GRID_SIZE = 16;
 
 for (let i = 0; i < GRID_SIZE; i++) {
   const innerContainer = document.createElement("div");
+  innerContainer.className = "inner-container";
   gridContainer.appendChild(innerContainer);
 }
