5c3ba354c396ae129493336575fbab9323262a45
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-25 22:06:50 +0530

    Add variables in js to get grid height and width

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 const gridContainer = document.querySelector(".grid-container");
+
 const GRID_SIZE = 16;
+const GRID_WIDTH = getComputedStyle(document.body).getPropertyValue(
+  "--grid-width"
+);
+
+const GRID_HEIGHT = getComputedStyle(document.body).getPropertyValue(
+  "--grid-height"
+);
 
 for (let i = 0; i < GRID_SIZE; i++) {
   for (let j = 0; j < GRID_SIZE; j++) {
