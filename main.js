c320fc24d83fcbf3c87d40d3c12408e1aa1d0b10
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-26 11:15:11 +0530

    Added row container and column containers inside it

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 const gridContainer = document.querySelector(".grid-container");
 
-const GRID_SIZE = 16;
+const GRID_SIZE = 8;
+
 const GRID_WIDTH = Number(
   getComputedStyle(document.body)
     .getPropertyValue("--grid-width")
     .replace("px", "")
 );
+
 const GRID_HEIGHT = Number(
   getComputedStyle(document.body)
     .getPropertyValue("--grid-height")
     .replace("px", "")
 );
 
-console.log(GRID_HEIGHT);
-
 for (let i = 0; i < GRID_SIZE; i++) {
+  const rowContainer = document.createElement("div");
+  rowContainer.className = "row-container";
+
   for (let j = 0; j < GRID_SIZE; j++) {
-    const innerContainer = document.createElement("div");
-    innerContainer.className = "inner-container";
-    innerContainer.style.height = `${GRID_HEIGHT / GRID_SIZE}px`;
-    innerContainer.style.width = `${GRID_WIDTH / GRID_SIZE}px`;
-    gridContainer.appendChild(innerContainer);
+    const columnContainer = document.createElement("div");
+    columnContainer.className = "column-container";
+    columnContainer.style.height = `${GRID_HEIGHT / GRID_SIZE}px`;
+    columnContainer.style.width = `${GRID_WIDTH / GRID_SIZE}px`;
+    rowContainer.appendChild(columnContainer);
   }
+
+  gridContainer.appendChild(rowContainer);
 }
